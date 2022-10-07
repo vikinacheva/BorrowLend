0658916b40da57ffc26fba042531f0c1eda033b7
vikinacheva <vikinacheva04@gmail.com>
2022-10-07 09:46:09 +0300

    Message

--- BorrowLend/Controllers/ItemController.cs
+++ BorrowLend/Controllers/ItemController.cs
@@ -9,13 +9,66 @@ namespace BorrowLend.Controllers
         private readonly ApplicationDbContext _db;
         public IActionResult Index()
         {
-            IEnumerable<Item>
-                obj = _db.Items;
+            IEnumerable<Item> obj = _db.Items;
             return View(obj);
         }
         public ItemController(ApplicationDbContext db)
         {
             _db = db;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int?id)
+        {
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int?id)
+        {
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
